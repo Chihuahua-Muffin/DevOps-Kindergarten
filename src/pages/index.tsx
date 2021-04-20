374a1f505f7a760eb7e833b20fb7ec15c5abe92a
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2021-04-20 23:12:37 +0900

    Create-next-app 초기 설정
    1. typescript
    2. eslint
    3. Link 이용해서 index 페이지, roadmap 페이지 생성완료

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const IndexPage = () => (
+  <div>
+    Index 입니다.
+    <Link href="/roadmap">hi</Link>
+  </div>
+);
+
+export default IndexPage;
---
+++ src/pages/roadmap/index.tsx
@@ -0,0 +1,9 @@
+const RoadMapPage = () => {
+  return (
+    <div>
+      Road Map 페이지 입니다.
+    </div>
+  )
+}
+
+export default RoadMapPage;
