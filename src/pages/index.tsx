05ec8aa44e0ce7f829bd462616d9a6be7082c1ad
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2021-04-22 00:24:09 +0900

    index 페이지 수정
    랜딩 페이지로 사용 예정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
 import Link from 'next/link';
-import { ChakraProvider, Button } from "@chakra-ui/react";
+import { Flex, Box, Button, Center } from "@chakra-ui/react";
 
 const IndexPage = () => {
   return (
-    <ChakraProvider>
-      <div>
-        Index 입니다.
-      </div>
-      <Link href="/roadmap"><Button>hi</Button></Link>
-    </ChakraProvider>
+    <Box w="100%" h="800px" bg="cyan.300" textAlign="center">
+      <Flex h="100%" direction="column" align="center" justify="space-around">
+        <Center>랜딩 페이지</Center>
+        <Link href="/roadmap"><Button>시작하기</Button></Link>
+      </Flex>
+    </Box>
   );
 };
 
