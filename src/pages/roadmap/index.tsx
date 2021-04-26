d1f738b30832e4d41643dcef7cbb14b10f044061
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2021-04-26 20:41:37 +0900

    로드맵 페이지 생성

--- src/pages/roadmap/index.tsx
+++ src/pages/roadmap/index.tsx
@@ -1,9 +1,26 @@
+import { useRouter } from 'next/router';
+import {
+  chakra,
+  Box,
+} from '@chakra-ui/react';
+
+const Container = chakra(Box, {
+  baseStyle: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+  },
+});
+
 const RoadMapPage = () => {
+  const router = useRouter();
+
   return (
-    <div>
-      Road Map 페이지 입니다.
-    </div>
-  )
-}
+    <Container>
+      {router.pathname} 페이지 입니다.
+    </Container>
+  );
+};
 
 export default RoadMapPage;
