5ad95da9c29f5abc1890b749645ce263fb5b0f88
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2021-04-26 20:38:49 +0900

    Layout 컴포넌트 추가

---
+++ src/components/Layout.tsx
@@ -0,0 +1,19 @@
+import Header from '#/components/common/Header';
+import Head from 'next/head';
+
+// 모든 페이지에 적용되는 컴포넌트
+export const Layout: React.FunctionComponent = props =>
+  <div id="layout">
+    <Head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+    </Head>
+    <Header/>
+    <main>
+      {props.children}
+    </main>
+  </div>
+
+export default Layout;
