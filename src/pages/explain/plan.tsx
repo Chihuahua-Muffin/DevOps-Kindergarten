528fd351bbcaf1b4069a41d5a20c914afbe49a3a
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2021-05-10 20:44:11 +0900

    설명 페이지 생성

---
+++ src/pages/explain/plan.tsx
@@ -0,0 +1,25 @@
+import ExplainContainer from '#/components/explain/ExplainContainer';
+import ExplainTitle from '#/components/explain/ExplainTitle';
+import ExplainSubTitle from '#/components/explain/ExplainSubTitle';
+import ExplainContent from '#/components/explain/ExplainContent';
+
+// ExplainTitle     : h1
+// ExplainSubTitle  : h2   /#{id} 이런식으로 들어감
+// ExplainContent   : p
+
+const PlanPage = () => {
+  return (
+    <ExplainContainer>
+      <ExplainTitle>PLAN</ExplainTitle>
+      <ExplainContent>
+        PLAN?
+      </ExplainContent>
+      <ExplainSubTitle id="tools">Tools</ExplainSubTitle>
+      <ExplainContent>
+        대표 툴에 대한 설명
+      </ExplainContent>
+    </ExplainContainer>
+  );
+};
+
+export default PlanPage;
