e21de7b2f42fee2c7775d951ea75ed5a9a7d32a2
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2021-05-19 14:26:38 +0900

    아이디, 비밀번호 Validation 추가

--- src/components/signin/SignInValidation.ts
+++ src/components/signin/SignInValidation.ts
@@ -8,12 +8,14 @@ export default function SignUpValidation({ id, password }: SignInValidationProps
 
   if (!id) {
     errors.id = '아이디가 입력되지 않았습니다.';
+  } else if (id.length < 6) {
+    errors.id = '6자 이상의 아이디를 사용해야 합니다.';
   }
 
   if (!password) {
     errors.password = '비밀번호가 입력되지 않았습니다.';
-  } else if (password.length < 8) {
-    errors.password = '8자 이상의 패스워드를 사용해야 합니다.';
+  } else if (password.length < 6) {
+    errors.password = '6자 이상의 패스워드를 사용해야 합니다.';
   }
 
   return errors;
