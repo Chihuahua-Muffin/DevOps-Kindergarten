0aee2fe5ff68e3d11589d991a1f189861889db62
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2021-06-02 00:09:48 +0900

    CodeBox 컴포넌트 임시 생성

--- src/components/common/CodeBox.tsx
+++ src/components/common/CodeBox.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { chakra, Box, Button } from '@chakra-ui/react';
+import { chakra, Box, Button, Text } from '@chakra-ui/react';
+import { useToast } from "@chakra-ui/react"
+
+const toastId = 'TOAST';
 
 interface CodeBoxProps {
   text: string;
@@ -7,40 +10,70 @@ interface CodeBoxProps {
 
 const Container = chakra(Box, {
   baseStyle: {
-    display: 'flex',
     position: 'relative',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'gray',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+});
+
+const CodeContainer = chakra(Box, {
+  baseStyle: {
+    position: 'relative',
+    backgroundColor: 'gray.50',
     width: '600px',
-    height: '300px',
+    padding: '10px',
     borderRadius: '5px',
+    overflowX: 'scroll',
+    overflowY: 'hidden',
+  },
+});
+
+const Code = chakra(Text, {
+  baseStyle: {
+    fontSize: '14px',
+    whiteSpace: 'pre',
+    textAlign: 'start',
   },
 });
 
 const CopyButton = chakra(Button, {
   baseStyle: {
     position: 'absolute',
-    margin: '10px',
     top: '0',
     right: '0',
+    zIndex: '100',
+    transform: 'translateY(-100%)',
   },
 });
 
 const CodeBox = ({ text }: CodeBoxProps) => {
+  const toast = useToast();
+
   const onClickButton = () => {
     const tempElem = document.createElement('textarea');
     tempElem.value = text;
     document.body.appendChild(tempElem);
-
     tempElem.select();
     document.execCommand('copy');
     document.body.removeChild(tempElem);
+    if (!toast.isActive(toastId)) {
+      toast({
+        id: toastId,
+        title: '클립보드에 복사되었습니다.',
+        status: 'success',
+        duration: 2000,
+        position: 'bottom-left',
+        isClosable: true,
+      });
+    }
   };
+
   return (
     <Container>
-      <div>{text}</div>
-      <CopyButton onClick={onClickButton}>클립복사</CopyButton>
+      <CopyButton onClick={onClickButton}>복사하기</CopyButton>
+      <CodeContainer>
+        <Code as="pre">{text}</Code>
+      </CodeContainer>
     </Container>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,24 @@ import Link from 'next/link';
 import { Flex, Box, Button, Center } from '@chakra-ui/react';
 import CodeBox from '#/components/common/CodeBox';
 
+const TEXT = "const CopyButton = chakra(Button, dafdfdsfdsafadsfdsfdsfadsfadsfsdfasdfadsfsdafasdfdsfsdafds{\n\
+  baseStyle: {\n\
+    position: 'absolute',\n\
+    margin: '10px',\n\
+    top: '0',\n\
+    right: '0',\n\
+  },\n\
+});";
+
 const LandingPage = () => (
   <>
-    <Box w="100%" h="100vh" bg="cyan.200" textAlign="center">
+    <Box w="100%" h="100vh" bg="gray.100" textAlign="center">
       <Flex h="100%" direction="column" align="center" justify="space-around">
         <Center>랜딩 페이지</Center>
         <Link href="/roadmap">
           <Button>시작하기</Button>
         </Link>
-        <CodeBox text='console.log("콘솔로그입니다.")' />
+        <CodeBox text={TEXT} />
       </Flex>
     </Box>
   </>
