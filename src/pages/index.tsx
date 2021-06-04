3a352c70b6e289bf1d21adb214d405f1bb9a4bb4
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-06-04 21:14:54 +0900

    코드 박스 (클립보드복사기능) 컴포넌트 추가 (#34)
    * eslint 룰 추가
    
    * prettier 추가
    
    * CodeBox 컴포넌트 추가
    
    * CodeBox 컴포넌트 임시 생성
    
    * eslint 룰 적용
    
    * 버튼 색 바꾸기
    
    * 임시 컴포넌트 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Link from 'next/link';
-import {
-  Flex, Box, Button, Center,
-} from '@chakra-ui/react';
+import { Flex, Box, Button, Center } from '@chakra-ui/react';
 
 const LandingPage = () => (
   <>
-    <Box w="100%" h="100vh" bg="cyan.200" textAlign="center">
+    <Box w="100%" h="100vh" bg="gray.100" textAlign="center">
       <Flex h="100%" direction="column" align="center" justify="space-around">
         <Center>랜딩 페이지</Center>
-        <Link href="/roadmap"><Button>시작하기</Button></Link>
+        <Link href="/roadmap">
+          <Button>시작하기</Button>
+        </Link>
       </Flex>
     </Box>
   </>
