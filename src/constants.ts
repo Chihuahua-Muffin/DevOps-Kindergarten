04931777f22d7a6a6a6d5f326d3f237fa84191b7
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2021-06-09 17:10:00 +0900

    eslint 룰 적용

--- src/constants.ts
+++ src/constants.ts
@@ -3,8 +3,7 @@ export const HEADER_HEIGHT = '80px';
 
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 export const DEVELOPMENT_URL = 'http://localhost:3000';
-export const PRODUCTION_URL = 
-  'https://devops-kindergarten-website-devops-kindergarten.vercel.app' ||
-  'https://devops-kindergarten-website-git-main-devops-kindergarten.vercel.app' ||
-  'https://devops-kindergarten-website-rieg5rsyi-devops-kindergarten.vercel.app' ||
-  'https://devops-kindergarten-website.vercel.app';
+export const PRODUCTION_URL = 'https://devops-kindergarten-website-devops-kindergarten.vercel.app'
+  || 'https://devops-kindergarten-website-git-main-devops-kindergarten.vercel.app'
+  || 'https://devops-kindergarten-website-rieg5rsyi-devops-kindergarten.vercel.app'
+  || 'https://devops-kindergarten-website.vercel.app';
