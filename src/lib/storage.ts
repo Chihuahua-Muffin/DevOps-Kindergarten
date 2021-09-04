a9393bf776f7299da67a4cb19fad99dae7eef877
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-09-04 17:48:11 +0900

    storage lib 생성

---
+++ src/lib/storage.ts
@@ -0,0 +1,41 @@
+/*
+usage: 로컬 스토리지에 JSON 형태로 저장 / 불러오기 / 삭제 헬퍼
+storage.set('foo', 'bar');
+storage.set('foobar', { foo: 'bar' });
+let foo = storage.get('foo'); // bar
+storage.remove('foo');
+*/
+interface setProps {
+  key: string,
+  object: string | {[key: string]: string}
+}
+
+const storage = {
+  set: ({ key, object }: setProps) => {
+    if (!localStorage) return;
+    localStorage[key] = (typeof object) === 'string' ? object : JSON.stringify(object);
+  },
+  get: (key: string) => {
+    if (!localStorage) return null;
+
+    if (!localStorage[key]) {
+      return null;
+    }
+
+    try {
+      const parsed = JSON.parse(localStorage[key]);
+      return parsed;
+    } catch (e) {
+      return localStorage[key];
+    }
+  },
+  remove: (key: string) => {
+    if (!localStorage) return null;
+
+    if (localStorage[key]) {
+      localStorage.removeItem(key);
+    }
+  },
+};
+
+export default storage;
