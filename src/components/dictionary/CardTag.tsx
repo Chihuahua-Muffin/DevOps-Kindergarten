d74e4d09e9cf759ba33c193de840c4c3c6526a4b
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-09-18 22:29:34 +0900

    사전 관련 컴포넌트 전부 주석 처리

--- src/components/dictionary/CardTag.tsx
+++ src/components/dictionary/CardTag.tsx
@@ -1,42 +1,43 @@
-import React from 'react';
-import {
-  Box,
-  chakra,
-  Text,
-} from '@chakra-ui/react';
+// import React from 'react';
+// import {
+//   Box,
+//   chakra,
+//   Text,
+// } from '@chakra-ui/react';
 
-interface CardTagProps {
-  tags: string[];
-}
+// interface CardTagProps {
+//   tags: string[];
+// }
 
-const TagContainer = chakra(Box, {
-  baseStyle: {
-    display: 'flex',
-    position: 'absolute',
-    padding: '10px',
-    bottom: '0',
-    right: '0',
-  },
-});
+// const TagContainer = chakra(Box, {
+//   baseStyle: {
+//     display: 'flex',
+//     position: 'absolute',
+//     padding: '10px',
+//     bottom: '0',
+//     right: '0',
+//   },
+// });
 
-const Tag = chakra(Box, {
-  baseStyle: {
-    display: 'flex',
-    fontSize: '12px',
-    padding: '4px',
-    margin: '4px',
-  },
-});
+// const Tag = chakra(Box, {
+//   baseStyle: {
+//     display: 'flex',
+//     fontSize: '12px',
+//     padding: '4px',
+//     margin: '4px',
+//   },
+// });
 
-const CardTag = ({ tags }: CardTagProps) => (
-  <TagContainer>
-    {tags.map((tag) => (
-      <Tag key={tag}>
-        <Text color="teal.600" fontWeight="bold">#</Text>
-        <Text color="teal.400">{tag}</Text>
-      </Tag>
-    ))}
-  </TagContainer>
-);
+// const CardTag = ({ tags }: CardTagProps) => (
+//   <TagContainer>
+//     {tags.map((tag) => (
+//       <Tag key={tag}>
+//         <Text color="teal.600" fontWeight="bold">#</Text>
+//         <Text color="teal.400">{tag}</Text>
+//       </Tag>
+//     ))}
+//   </TagContainer>
+// );
 
-export default CardTag;
+// export default CardTag;
+export { };
--- src/components/dictionary/CreateButton.tsx
+++ src/components/dictionary/CreateButton.tsx
@@ -1,31 +1,32 @@
-import React from 'react';
-import {
-  chakra,
-  IconButton,
-} from '@chakra-ui/react';
-import Link from 'next/link';
-import AddIcon from '@material-ui/icons/Add';
+// import React from 'react';
+// import {
+//   chakra,
+//   IconButton,
+// } from '@chakra-ui/react';
+// import Link from 'next/link';
+// import AddIcon from '@material-ui/icons/Add';
 
-const CreateButtonContainer = chakra(IconButton, {
-  baseStyle: {
-    position: 'fixed',
-    bottom: '0',
-    right: '0',
-    margin: '20px',
-    fontSize: '24px',
-    fontWeight: '600',
-  },
-});
+// const CreateButtonContainer = chakra(IconButton, {
+//   baseStyle: {
+//     position: 'fixed',
+//     bottom: '0',
+//     right: '0',
+//     margin: '20px',
+//     fontSize: '24px',
+//     fontWeight: '600',
+//   },
+// });
 
-const CreateButton = () => (
-  <Link href="/dictionary/create">
-    <CreateButtonContainer
-      icon={<AddIcon />}
-      colorScheme="teal"
-      size="lg"
-      isRound
-    />
-  </Link>
-);
+// const CreateButton = () => (
+//   <Link href="/dictionary/create">
+//     <CreateButtonContainer
+//       icon={<AddIcon />}
+//       colorScheme="teal"
+//       size="lg"
+//       isRound
+//     />
+//   </Link>
+// );
 
-export default CreateButton;
+// export default CreateButton;
+export { };
