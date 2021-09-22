7716c8e48229327c68be5816b9155996167bd492
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-09-22 14:20:10 +0900

    이론 페이지에 댓글 리스트 컴포넌트 추가

--- src/components/common/comment/CommentList.tsx
+++ src/components/common/comment/CommentList.tsx
@@ -9,13 +9,14 @@ import { CONTENT_WIDTH } from '#/constants';
 const CommentListContainer = chakra(Box, {
   baseStyle: {
     display: 'flex',
+    margin: 'auto',
     flexDirection: 'column',
     width: CONTENT_WIDTH,
   },
 });
 
 // 댓글 컨테이너로, 데이터들을 Comment에 넘겨주어야 함.
-// Axios 연동되면 구현
+// Todo: props로 url 넘어오면 axios로 데이터 받아와서 코멘트 출력
 const CommentList = () => (
   <CommentListContainer>
     <Comment />
