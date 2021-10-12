6265d205da127e6711bd45dcc8f41deaac4988a7
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-10-12 17:22:18 +0900

    유저 변수 추가 (#76)
    * axios default URL & withCredentials 옵션 메인으로 옮기기
    
    * 유저 변수 추가

--- src/lib/api/auth.tsx
+++ src/lib/api/auth.tsx
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.NEXT_PUBLIC_DEV_SERVER_URL;
-axios.defaults.withCredentials = true; // 토큰 쿠키 받기
-
 const LOGIN_API_URL = '/api/login';
 const LOGOUT_API_URL = '/api/logout';
 const REFRESH_API_URL = '/api/refresh';
