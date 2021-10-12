299509d1b12bf652f424d943670505eb82ae2b75
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-10-12 16:59:23 +0900

    Update HeaderNavigation.tsx

--- src/components/common/header/HeaderNavigation.tsx
+++ src/components/common/header/HeaderNavigation.tsx
@@ -1,26 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
 import {
   chakra,
   Spacer,
   Box,
   Button,
 } from '@chakra-ui/react';
-import { useSelector } from 'react-redux';
 
-import { useAppDispatch } from '#/hooks/useRedux';
-import { logoutAsync, loginAsync } from '#/redux/ducks/auth';
-import { logoutAPI, loginAPI } from '#/lib/api/auth';
-
+import { AppState } from '#/redux/store';
+import LoginStatusMenu from './LoginStatusMenu';
 import {
   LOGIN_PAGE_URL,
   LECTURE_PAGE_URL,
   ROADMAP_PAGE_URL,
-  // DICTIONARY_PAGE_URL, 사전 페이지 삭제
 } from '#/constants';
-import { AppState } from '#/redux/store';
-import LoginStatusMenu from './LoginStatusMenu';
 
 const NavigationContainer = chakra(Box, {
   baseStyle: {
@@ -38,8 +33,6 @@ const NavItem = chakra(Button, {
 });
 
 const HeaderNavigation = () => {
-  // temp
-  const dipatch = useAppDispatch();
   const router = useRouter();
   const loginState = useSelector((state: AppState) => state.auth);
   const [select, setSelected] = useState(router.pathname);
@@ -47,26 +40,6 @@ const HeaderNavigation = () => {
   useEffect(() => {
     setSelected(router.pathname);
   }, [router]);
-
-  const onClickLogoutTemp = async () => {
-    try {
-      // const data = await dipatch(logoutAsync('admin'));
-      const data = await logoutAPI('admin');
-      console.log('logout', data);
-    } catch (error) {
-      console.log('logout error', error);
-    }
-  };
-
-  const onClickLoginTemp = async () => {
-    try {
-      // const data = await dipatch(loginAsync({ username: 'admin', password: 'admin' }));
-      const data = await loginAPI({ username: 'admin', password: 'admin' });
-      console.log('loginAPI', data);
-    } catch (error) {
-      console.log('loginAPI error', error);
-    }
-  };
 
   return (
     // SEO를 위한 HTML 태그
@@ -87,12 +60,6 @@ const HeaderNavigation = () => {
           실습
         </NavItem>
       </Link>
-      <NavItem onClick={onClickLogoutTemp} colorScheme="teal" variant="ghost">
-        로그아웃테스트
-      </NavItem>
-      <NavItem onClick={onClickLoginTemp} colorScheme="teal" variant="ghost">
-        로그인테스트
-      </NavItem>
       {loginState.isLogin
         ? <LoginStatusMenu />
         : (
