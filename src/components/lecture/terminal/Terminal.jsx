b54d0df95391830fd1ccdd9ab4eba1134a79e48e
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-11-18 23:55:51 +0900

    [#69] width에 새로 터미널을 생성하지 않고 스타일만 변경 했습니다.

--- src/components/lecture/terminal/Terminal.jsx
+++ src/components/lecture/terminal/Terminal.jsx
@@ -12,19 +12,10 @@ const Container = chakra(Box, {
   baseStyle: {
     position: 'fixed',
     bottom: 0,
-    left: '300px',
-    width: 'calc(100vw - 300px)',
     height: '30vh',
     color: 'white',
     backgroundColor: 'black',
     paddingLeft: '10px',
-  },
-});
-
-const SmallContainer = chakra(Container, {
-  baseStyle: {
-    left: '70px',
-    width: 'calc(100vw - 70px)',
   },
 });
 
@@ -40,14 +31,13 @@ const Terminal = () => {
     console.log('code', code);
 
     // 한글 입력 시
-    if (code >= 0x1100 && code <= 0x11FF) { console.log(code, '한글입력'); return; }
-    if (code >= 0x3130 && code <= 0x318F) { console.log(code, '한글입력'); return; }
-    if (code >= 0xAC00 && code <= 0xD7A3) { console.log(code, '한글입력'); return; }
+    if (code >= 0x1100 && code <= 0x11FF) return;
+    if (code >= 0x3130 && code <= 0x318F) return;
+    if (code >= 0xAC00 && code <= 0xD7A3) return;
 
     // 백 스페이스
     if (code === 127) {
       if (!buffer) return;
-      console.log('백 스페이스 입력');
       setBuffer(buffer.slice(0, -1));
       xtermRef.current.terminal.write('\b \b');
       return;
@@ -64,22 +54,6 @@ const Terminal = () => {
     setBuffer((prevState) => prevState + string);
     xtermRef.current.terminal.write(string);
   };
-
-  // // 터미널 키 입력
-  // const onKey = (event) => {
-  //   const string = event.key;
-  //   const code = event.domEvent.keyCode;
-
-  //   if (string === '\r') {
-  //     // enter
-  //     socketClient.current.emit(EMIT_CHAT_MESSAGE, buffer.trim());
-  //     setBuffer('');
-  //   } else if (event.domEvent.keyCode === 8) {
-  //     // back-space
-  //     setBuffer(buffer.slice(0, -1));
-  //     xtermRef.current.terminal.write(buffer.slice(0, -1));
-  //   }
-  // };
 
   useEffect(() => {
     // 소켓 생성 및 연결
@@ -108,19 +82,13 @@ const Terminal = () => {
   };
 
   return (
-    <>
-      {islargerthan1100 ? (
-        <Container>
-          {/* Create a new terminal and set it's ref. */}
-          <XTerm options={options} ref={xtermRef} onData={onData} />
-        </Container>
-      ) : (
-        <SmallContainer>
-          {/* Create a new terminal and set it's ref. */}
-          <XTerm options={options} ref={xtermRef} onData={onData} />
-        </SmallContainer>
-      )}
-    </>
+    <Container
+      width={islargerthan1100 ? 'calc(100vw - 300px)' : 'calc(100vw - 70px)'}
+      left={islargerthan1100 ? '300px' : '70px'}
+    >
+      {/* Create a new terminal and set it's ref. */}
+      <XTerm options={options} ref={xtermRef} onData={onData} />
+    </Container>
   );
 };
 
