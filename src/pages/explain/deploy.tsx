5f7469fafebb7cbe45c8de25fd19f5cb1dc9c38c
SeokyungKim <kskyung0624@gmail.com>
2021-11-20 10:24:58 +0900

    add deploy page explaination

--- src/pages/explain/deploy.tsx
+++ src/pages/explain/deploy.tsx
@@ -5,18 +5,42 @@ import ExplainLayout from '#/components/layouts/Explain';
 
 import ExplainTitle from '#/components/explain/ExplainTitle';
 import ExplainContent from '#/components/explain/ExplainContent';
+import ExplainTool from '#/components/explain/ExplainTool';
 
 const DeployPage = () => (
   <>
     <TableOfContents />
     <ExplainTitle id="deploy">Deploy</ExplainTitle>
     <ExplainContent>
-      DEPLOY?
+      제품 릴리즈를 운영 환경으로 관리, 조정, 예약 및 자동화하는 단계입니다.
     </ExplainContent>
+
     <ExplainTitle id="tools">Tools</ExplainTitle>
-    <ExplainContent>
-      대표 툴에 대한 설명
-    </ExplainContent>
+    <ExplainTool
+      title="Docker"
+      src="/docker.png"
+      titleHref="https://www.docker.com/"
+      explaination={(
+        <>
+          Docker는 애플리케이션을 신속하게 구축, 테스트 및 배포할 수 있는 소프트웨어 플랫폼입니다.
+          Docker를 사용하면 환경에 구애받지 않고 애플리케이션을 신속하게 배포 및 확장할 수 있으며 코드가 문제없이 실행될 것임을 확신할 수 있습니다.
+        </>
+      )}
+      features={['컨테이너', '환경 표준화', '데이터 처리', '패키징']}
+    />
+
+    <ExplainTool
+      title="AWS"
+      src="/aws.png"
+      titleHref="https://aws.amazon.com/"
+      explaination={(
+        <>
+          클라우드 서비스는 사용자의 직접적인 관리 없이 시스템 리소스를 필요 시 바로 제공하는 서비스를 말합니다.
+          대표적인 제품에는 AWS가 있습니다.
+        </>
+      )}
+      features={['on-demand', 'SaaS', 'PaaS', 'IaaS']}
+    />
   </>
 );
 
--- src/pages/explain/plan.tsx
+++ src/pages/explain/plan.tsx
@@ -20,34 +20,48 @@ const PlanPage = () => (
       title="Jira"
       src="/jira.jpeg"
       titleHref="https://www.atlassian.com/software/jira"
-      explaination={(
+      explaination={
         <>
-          지라(JIRA)는 아틀라시안이 개발한 사유 이슈 추적 제품이다. 버그 추적, 이슈 추적, 프로젝트 관리 기능을 제공하는 소프트웨어입니다.
+          지라(JIRA)는 아틀라시안이 개발한 사유 이슈 추적 제품이다. 버그 추적,
+          이슈 추적, 프로젝트 관리 기능을 제공하는 소프트웨어입니다.
         </>
-      )}
-      features={['스크럼 보드 제공', '칸반 보드 제공', '애자일 보고', '버그 및 이슈 트랙커 기능']}
+      }
+      features={[
+        '스크럼 보드 제공',
+        '칸반 보드 제공',
+        '애자일 보고',
+        '버그 및 이슈 트랙커 기능',
+      ]}
     />
     <ExplainTool
       title="Git"
       src="/git.png"
       titleHref="https://git-scm.com/"
-      explaination={(
+      explaination={
         <>
-          깃(Git /ɡɪt/)은 컴퓨터 파일의 변경사항을 추적하고 여러 명의 사용자들 간에 해당 파일들의 작업을 조율하기 위한 분산 버전 관리 시스템입니다.
-          소프트웨어 개발에서 소스 코드 관리에 주로 사용되지만 어떠한 집합의 파일의 변경사항을 지속적으로 추적하기 위해 사용될 수 있습니다.
-          기하학적 불변 이론을 바탕으로 설계됐고, 분산 버전 관리 시스템으로서 빠른 수행 속도에 중점을 두고 있는 것이 특징이며
-          데이터 무결성, 분산, 비선형 워크플로를 지원합니다.
+          깃(Git /ɡɪt/)은 컴퓨터 파일의 변경사항을 추적하고 여러 명의 사용자들
+          간에 해당 파일들의 작업을 조율하기 위한 분산 버전 관리 시스템입니다.
+          소프트웨어 개발에서 소스 코드 관리에 주로 사용되지만 어떠한 집합의
+          파일의 변경사항을 지속적으로 추적하기 위해 사용될 수 있습니다.
+          기하학적 불변 이론을 바탕으로 설계됐고, 분산 버전 관리 시스템으로서
+          빠른 수행 속도에 중점을 두고 있는 것이 특징이며 데이터 무결성, 분산,
+          비선형 워크플로를 지원합니다.
         </>
-      )}
-      features={['가지 치지(Branch)와 병합(Merge)', '가볍고 빠르다', '분산 작업', '데이터 보장', '준비 영역(Staging area)', '오픈 소스']}
+      }
+      features={[
+        '가지 치지(Branch)와 병합(Merge)',
+        '가볍고 빠르다',
+        '분산 작업',
+        '데이터 보장',
+        '준비 영역(Staging area)',
+        '오픈 소스',
+      ]}
     />
   </>
 );
 
 PlanPage.getLayout = (page: React.ReactElement) => (
-  <ExplainLayout>
-    {page}
-  </ExplainLayout>
+  <ExplainLayout>{page}</ExplainLayout>
 );
 
 export default PlanPage;
