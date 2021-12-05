abfec6ce19c16203602d42e81978b8b4872dcfaa
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-12-05 15:08:02 +0900

    feat: 도커로 MySQL 실행 실습 생성

--- src/components/lecture/contents/index.tsx
+++ src/components/lecture/contents/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 import One, { Checkpoints as OneCheckpoints, Title as OneTitle, lectureNumber as OneLectureNumaber } from './1';
 import Two, { Checkpoints as TwoCheckpoints, Title as TwoTitle, lectureNumber as TwoLectureNumaber } from './2';
+import Three, { Checkpoints as ThreeCheckpoints, Title as ThreeTitle, lectureNumber as ThreeLectureNumaber } from './3';
 import type { LectureContent } from './types';
 
 const LectureContents: LectureContent[] = [
@@ -24,6 +25,17 @@ const LectureContents: LectureContent[] = [
       tags: ['Docker'],
       description: '도커와 도커의 설치와 간단한 사용법에 대해서 알아봅시다.',
       image: 'docker.png',
+    },
+  },
+  {
+    lectureNumber: ThreeLectureNumaber,
+    sliderContents: Three,
+    checkpoints: ThreeCheckpoints,
+    card: {
+      title: ThreeTitle,
+      tags: ['Docker', 'MySQL'],
+      description: '도커로 MySQL을 실행해봅니다.',
+      image: 'mysql.png',
     },
   },
 ];
